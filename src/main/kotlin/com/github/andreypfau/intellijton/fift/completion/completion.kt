a154202a1779f4ada3f5d2308045988f74bffecb
Andrey Pfau <a.pfau@adguard.com>
2022-02-14 06:16:20 +0300

    fift completions

--- src/main/kotlin/com/github/andreypfau/intellijton/fift/completion/completion.kt
+++ src/main/kotlin/com/github/andreypfau/intellijton/fift/completion/completion.kt
@@ -1,16 +1,39 @@
 package com.github.andreypfau.intellijton.fift.completion
 
 import com.github.andreypfau.intellijton.fift.psi.FiftFile
-import com.github.andreypfau.intellijton.fift.psi.FiftTypes
+import com.github.andreypfau.intellijton.fift.psi.FiftTypes.*
 import com.github.andreypfau.intellijton.fift.resolve.resolveAllWordDefs
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.editor.EditorModificationUtil
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
 
 class FiftWordCompletionContributor : CompletionContributor() {
     init {
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(FiftTypes.IDENTIFIER), FuncWordCompletionProvider())
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(IDENTIFIER), FuncBasicWordCompletionProvider())
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(IDENTIFIER), FuncWordCompletionProvider())
+    }
+}
+
+class FuncBasicWordCompletionProvider : CompletionProvider<CompletionParameters>() {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        if (parameters.position.text == CompletionInitializationContext.DUMMY_IDENTIFIER_TRIMMED) return
+        sequenceOf(
+            INCLUDE,
+            IF, IFNOT, COND,
+            TRUE, FALSE,
+            DUP, DROP, SWAP, ROT, REV_ROT, OVER, TUCK, NIP, DUP_DUP,
+            DROP_DROP, SWAP_SWAP, PICK, ROLL, REV_ROLL, EXCH, EXCH2, COND_DUP
+        ).map {
+            LookupElementBuilder.create(it).insertSpace()
+        }.forEach {
+            result.addElement(it)
+        }
     }
 }
 
@@ -26,9 +49,14 @@ class FuncWordCompletionProvider : CompletionProvider<CompletionParameters>() {
         file.resolveAllWordDefs().filter {
             it.wordDefIdentifier.textOffset < textOffset
         }.map { wordDef ->
-            LookupElementBuilder.createWithIcon(wordDef)
+            LookupElementBuilder.createWithIcon(wordDef).insertSpace()
         }.forEach {
             result.addElement(it)
         }
     }
+}
+
+fun LookupElementBuilder.insertSpace() = withInsertHandler { ctx, _ ->
+    ctx.document.insertString(ctx.selectionEndOffset, " ")
+    EditorModificationUtil.moveCaretRelatively(ctx.editor, 1)
 }

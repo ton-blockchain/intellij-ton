593982f2fe49843e6b1ab6bd314ec368d57c51c0
Andrey Pfau <a.pfau@adguard.com>
2022-02-23 23:38:18 +0300

    disable missing type warning

--- src/main/kotlin/com/github/andreypfau/intellijton/tlb/ide/annotation.kt
+++ src/main/kotlin/com/github/andreypfau/intellijton/tlb/ide/annotation.kt
@@ -1,7 +1,6 @@
 package com.github.andreypfau.intellijton.tlb.ide
 
 import com.github.andreypfau.intellijton.tlb.psi.*
-import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
@@ -48,46 +47,13 @@ class TlbAnnotator : Annotator {
                         .range(element)
                         .textAttributes(TlbColor.IMPLICIT_FIELD_NAME.textAttributesKey)
                         .create()
-                    else -> holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved reference: ${element.text}")
-                        .range(element)
-                        .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
-                        .create()
+                    // TODO: Enable after add include support
+//                    else -> holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved reference: ${element.text}")
+//                        .range(element)
+//                        .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
+//                        .create()
                 }
             }
-//            is TlbCombinator -> {
-//                val parent = element.parent as TlbCombinatorDeclaration
-//                val fields = parent.resolveFields()
-//                element.typeExpressionList.forEach { typeExpression ->
-//                    val referenceIdentifier = typeExpression.namedRef?.identifier ?: return@forEach
-//                    val foundField = fields.any { field ->
-//                        field.fieldName.textMatches(referenceIdentifier)
-//                    }
-//                    if (foundField) {
-//                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-//                            .range(typeExpression)
-//                            .textAttributes(TlbColor.FIELD_NAME.textAttributesKey)
-//                            .create()
-//                    } else {
-//                        val implicitFields = parent.resolveImplicitFields()
-//                        val foundImplicitField = implicitFields.any { implicitField ->
-//                            implicitField.implicitFieldName.textMatches(referenceIdentifier)
-//                        }
-//                        if (foundImplicitField) {
-//                            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-//                                .range(typeExpression)
-//                                .textAttributes(TlbColor.IMPLICIT_FIELD_NAME.textAttributesKey)
-//                                .create()
-//                        } else {
-//                            holder.newAnnotation(
-//                                HighlightSeverity.ERROR,
-//                                "Unresolved field: ${referenceIdentifier.text}"
-//                            ).range(typeExpression)
-//                                .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
-//                                .create()
-//                        }
-//                    }
-//                }
-//            }
         }
     }
 }

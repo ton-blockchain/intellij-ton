b3e27dc4bed7ee9287704d1a0d7d505a1e520056
Andrey Pfau <andreypfau@ton.org>
2022-04-30 19:21:41 +0300

    CI/CD

--- src/main/kotlin/org/ton/intellij/ide/spellchecker/TonDictionaryProvider.kt
+++ src/main/kotlin/org/ton/intellij/ide/spellchecker/TonDictionaryProvider.kt
@@ -3,5 +3,5 @@ package org.ton.intellij.ide.spellchecker
 import com.intellij.spellchecker.BundledDictionaryProvider
 
 class TonDictionaryProvider : BundledDictionaryProvider {
-    override fun getBundledDictionaries(): Array<String> = arrayOf("/ton.dic")
+    override fun getBundledDictionaries(): Array<String> = arrayOf("/org.ton.intellij/spelling/ton.dic")
 }

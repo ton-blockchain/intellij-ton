1d1a1159a4ba40052438a6753776ebe17452059b
Andrey Pfau <andreypfau@ton.org>
2023-12-04 01:12:59 +0700

    fix #122

--- src/main/kotlin/org/ton/intellij/func/inspection/FuncMissingReturnInspection.kt
+++ src/main/kotlin/org/ton/intellij/func/inspection/FuncMissingReturnInspection.kt
@@ -18,25 +18,31 @@ class FuncMissingReturnInspection : FuncInspectionBase() {
         val block = function.blockStatement ?: return
         val atomicType = function.type as? FuncAtomicType ?: return
         val isVoid = atomicType is FuncHoleType || (atomicType as? FuncTensorType)?.typeList?.isEmpty() == true
-        if (isVoid || isTerminating(block)) return
-        val brace = block.rBrace
-        holder.registerProblem(brace ?: block, "Missing return at end of function")
+        if (isVoid) return
+        holder.isTerminating(block)
     }
 
-    private fun isTerminating(element: FuncElement?): Boolean {
+    private fun ProblemsHolder.isTerminating(element: FuncElement?): Boolean {
         ProgressIndicatorProvider.checkCanceled()
         return when (element) {
             null -> false
             is FuncReturnStatement -> true
-            is FuncBlockStatement -> isTerminating(element.statementList.lastOrNull())
+            is FuncBlockStatement -> {
+                val result = isTerminating(element.statementList.lastOrNull())
+                if (!result) {
+                    val brace = element.rBrace
+                    registerProblem(brace ?: element, "Missing `return`")
+                }
+                result
+            }
             is FuncIfStatement -> isTerminating(element.blockStatement) && isTerminating(
                 element.`else` ?: element.elseIf
             )
-
             is FuncElse -> isTerminating(element.blockStatement)
             is FuncElseIf ->
                 isTerminating(element.blockStatement) && isTerminating(element.`else` ?: element.elseIf)
-
+            is FuncTryStatement -> isTerminating(element.blockStatement) && isTerminating(element.catch)
+            is FuncCatch -> isTerminating(element.blockStatement)
             else -> false
         }
     }

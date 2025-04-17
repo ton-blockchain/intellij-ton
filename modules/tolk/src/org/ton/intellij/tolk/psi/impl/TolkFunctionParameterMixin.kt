5c6ef0a0665a90e4cdec558dbc3ce1bb95bd6fab
andreypfau <andreypfau@ton.org>
2025-04-17 20:06:13 +0900

    fix self parameter resolving

--- modules/tolk/src/org/ton/intellij/tolk/psi/impl/TolkFunctionParameterMixin.kt
+++ modules/tolk/src/org/ton/intellij/tolk/psi/impl/TolkFunctionParameterMixin.kt
@@ -3,7 +3,9 @@ package org.ton.intellij.tolk.psi.impl
 import com.intellij.lang.ASTNode
 import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.stubs.IStubElementType
+import com.intellij.psi.util.PsiTreeUtil
 import org.ton.intellij.tolk.TolkIcons
+import org.ton.intellij.tolk.psi.TolkFunction
 import org.ton.intellij.tolk.psi.TolkParameter
 import org.ton.intellij.tolk.stub.TolkParameterStub
 import org.ton.intellij.tolk.type.TolkType
@@ -16,7 +18,15 @@ abstract class TolkParameterMixin : TolkNamedElementImpl<TolkParameterStub>, Tol
 
     override fun getIcon(flags: Int): Icon = TolkIcons.PARAMETER
 
-    override val type: TolkType? get() = typeExpression?.type
+    override val type: TolkType?
+        get() {
+            val typeExpression = typeExpression
+            if (typeExpression == null && name == "self") {
+                val function = PsiTreeUtil.getParentOfType(this, TolkFunction::class.java) ?: return null
+                return function.functionReceiver?.typeExpression?.type
+            }
+            return typeExpression?.type
+        }
 
     val isMutable: Boolean get() = stub?.isMutable ?: (mutateKeyword != null)
 
@@ -25,5 +35,6 @@ abstract class TolkParameterMixin : TolkNamedElementImpl<TolkParameterStub>, Tol
     }
 }
 
-val TolkParameter.isMutable: Boolean get() =
-    (this as? TolkParameterMixin)?.isMutable ?: (mutateKeyword != null)
+val TolkParameter.isMutable: Boolean
+    get() =
+        (this as? TolkParameterMixin)?.isMutable ?: (mutateKeyword != null)

c115440e7d25e36bcd4a55f15dc4367a8696bcd9
andreypfau <andreypfau@ton.org>
2025-05-03 13:51:57 +0900

    fix `self` resolving

---
+++ modules/tolk/src/org/ton/intellij/tolk/psi/impl/TolkSelfTypeExpressionMixin.kt
@@ -0,0 +1,29 @@
+package org.ton.intellij.tolk.psi.impl
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiReferenceBase
+import com.intellij.psi.util.parentOfType
+import org.ton.intellij.tolk.psi.TolkFunction
+import org.ton.intellij.tolk.psi.TolkSelfTypeExpression
+import org.ton.intellij.tolk.psi.TolkTypeExpression
+import org.ton.intellij.tolk.type.TolkType
+
+abstract class TolkSelfTypeExpressionMixin(node: ASTNode) : ASTWrapperPsiElement(node), TolkSelfTypeExpression {
+    override val type: TolkType
+        get() = reference.resolve()?.type ?: TolkType.Unknown
+
+    override fun getReference() = SelfTypeReference(this)
+
+    class SelfTypeReference(
+        element: TolkSelfTypeExpression
+    ) : PsiReferenceBase<TolkSelfTypeExpression>(
+        element, TextRange(0, element.textLength), false
+    ) {
+        override fun resolve(): TolkTypeExpression? {
+            val function = element.parentOfType<TolkFunction>()
+            return function?.functionReceiver?.typeExpression
+        }
+    }
+}

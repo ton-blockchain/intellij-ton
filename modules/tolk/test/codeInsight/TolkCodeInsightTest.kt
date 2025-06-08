b1869e5e537f98c590b44a990bf06f925f042c10
andreypfau <andreypfau@ton.org>
2025-06-08 17:29:21 +0900

    fix annotation arguments

--- modules/tolk/test/codeInsight/TolkCodeInsightTest.kt
+++ modules/tolk/test/codeInsight/TolkCodeInsightTest.kt
@@ -4,4 +4,6 @@ class TolkCodeInsightTest : TolkCodeInsightBaseTest() {
     override fun getTestDataPath(): String = "${super.testDataPath}/tests"
 
     fun `test a-tests`() = doTest()
+    fun `test allow-post-modification`() = doTest()
+    fun `test annotations-tests`() = doTest()
 }

a0f3628623b2a89cc1c89522651a0232c6594b55
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-07-29 19:34:48 +0400

    feat(func/ide): add search by project symbols (#289)
    Fixes #288

--- modules/func/src/org/ton/intellij/func/psi/FuncNamedElement.kt
+++ modules/func/src/org/ton/intellij/func/psi/FuncNamedElement.kt
@@ -1,8 +1,9 @@
 package org.ton.intellij.func.psi
 
+import com.intellij.navigation.NavigationItem
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
 
-interface FuncNamedElement : PsiNameIdentifierOwner, FuncElement {
+interface FuncNamedElement : PsiNameIdentifierOwner, FuncElement, NavigationItem {
     val identifier: PsiElement?
 }

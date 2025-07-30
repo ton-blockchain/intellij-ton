3e4e30955990cb983fe839afaab56dd27b8ae57e
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-07-30 14:56:37 +0400

    feat(tlb/resolving): add all rule definitions in the go-to-definition popup for types (#301)
    Fixes #300

--- modules/tlb/src/org/ton/intellij/tlb/TlbIcons.kt
+++ modules/tlb/src/org/ton/intellij/tlb/TlbIcons.kt
@@ -1,7 +1,12 @@
 package org.ton.intellij.tlb
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
 object TlbIcons {
     val FILE = IconLoader.getIcon("/icons/tlb.svg", TlbIcons::class.java)
+
+    val TYPE = AllIcons.Nodes.Type
+    val FIELD = AllIcons.Nodes.Field
+    val CONSTRUCTOR = AllIcons.Nodes.Constructor
 }

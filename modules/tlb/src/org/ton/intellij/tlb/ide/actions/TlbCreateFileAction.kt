45ad380e05f54796db3c5a4ab40e5100b8a7a05c
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-07-31 16:17:29 +0400

    feat(tlb/actions): add new TL-B file action (#350)
    Fixes #349

--- modules/tlb/src/org/ton/intellij/tlb/ide/actions/TlbCreateFileAction.kt
+++ modules/tlb/src/org/ton/intellij/tlb/ide/actions/TlbCreateFileAction.kt
@@ -8,20 +8,18 @@ import com.intellij.openapi.ui.InputValidatorEx
 import com.intellij.psi.PsiDirectory
 import org.ton.intellij.tlb.TlbIcons
 
-private const val CAPTION = "TL-B Schema"
-private const val DESCRIPTION = "Creates new TL-B schema"
-private const val TLB_FILE_TEMPLATE = "TL-B empty schema"
-
-class TlbCreateFileAction : CreateFileFromTemplateAction(CAPTION, DESCRIPTION, TlbIcons.FILE), DumbAware {
-    override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?) = CAPTION
+class TlbCreateFileAction : CreateFileFromTemplateAction(
+    "TL-B Schema", "Creates new TL-B schema", TlbIcons.FILE
+), DumbAware {
+    override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?) = "TL-B Schema"
 
     override fun buildDialog(
         project: Project,
         directory: PsiDirectory,
         builder: CreateFileFromTemplateDialog.Builder,
     ): Unit = builder.run {
-        setTitle(CAPTION)
-        addKind(CAPTION, TlbIcons.FILE, TLB_FILE_TEMPLATE)
+        setTitle("New TL-B Schema")
+        addKind("Empty file", TlbIcons.FILE, "TL-B empty schema")
         setValidator(object : InputValidatorEx {
             override fun getErrorText(inputString: String?): String? =
                 if (inputString.isNullOrEmpty()) {

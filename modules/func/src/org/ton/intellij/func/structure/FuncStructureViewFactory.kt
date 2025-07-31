af8ce775a50748d11ebc517650b21aed43dbe8a4
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-07-31 15:08:49 +0400

    feat(func/structure-view): show includes in structure view (#345)
    Fixes #344

--- modules/func/src/org/ton/intellij/func/structure/FuncStructureViewFactory.kt
+++ modules/func/src/org/ton/intellij/func/structure/FuncStructureViewFactory.kt
@@ -8,11 +8,14 @@ import com.intellij.lang.PsiStructureViewFactory
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import org.ton.intellij.func.FuncIcons
 import org.ton.intellij.func.psi.FuncConstVar
 import org.ton.intellij.func.psi.FuncFile
 import org.ton.intellij.func.psi.FuncFunction
 import org.ton.intellij.func.psi.FuncGlobalVar
+import org.ton.intellij.func.psi.FuncIncludeDefinition
 import org.ton.intellij.func.psi.FuncNamedElement
+import javax.swing.Icon
 
 class FuncStructureViewFactory : PsiStructureViewFactory {
     override fun getStructureViewBuilder(psiFile: PsiFile): StructureViewBuilder {
@@ -54,6 +57,10 @@ class FuncStructureViewFactory : PsiStructureViewFactory {
                 return element.name
             }
 
+            if (element is FuncIncludeDefinition) {
+                return element.text
+            }
+
             if (element is FuncFunction) {
                 val name = element.name
                 val parameterList = element.functionParameterList
@@ -80,6 +87,14 @@ class FuncStructureViewFactory : PsiStructureViewFactory {
             }
 
             return null
+        }
+
+        override fun getIcon(open: Boolean): Icon? {
+            if (element is FuncIncludeDefinition) {
+                return FuncIcons.FILE
+            }
+
+            return super.getIcon(open)
         }
     }
 }

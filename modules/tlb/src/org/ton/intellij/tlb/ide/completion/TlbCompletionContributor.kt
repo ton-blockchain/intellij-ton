a3212f3896946ff9c5289360243b3b97744a8f5d
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-08-25 15:53:05 +0400

    feat(tlb/completion): add completion for types (#394)
    Fixes #393

--- modules/tlb/src/org/ton/intellij/tlb/ide/completion/TlbCompletionContributor.kt
+++ modules/tlb/src/org/ton/intellij/tlb/ide/completion/TlbCompletionContributor.kt
@@ -2,10 +2,12 @@ package org.ton.intellij.tlb.ide.completion
 
 import com.intellij.codeInsight.completion.*
 import org.ton.intellij.tlb.ide.completion.providers.TolkBuiltinTypesCompletionProvider
+import org.ton.intellij.tlb.ide.completion.providers.TolkTypesCompletionProvider
 
 class TlbCompletionContributor : CompletionContributor() {
     init {
         extend(TolkBuiltinTypesCompletionProvider)
+        extend(TolkTypesCompletionProvider)
     }
 
     fun extend(provider: TlbCompletionProvider) {

5836c7b8b7cf38cc871242414aae40d3356bb7d9
i582 <51853996+i582@users.noreply.github.com>
2025-08-25 15:43:04 +0400

    fix(tlb/find-usages): fix element kind name in find usages
    Fixes #391

--- modules/tlb/src/org/ton/intellij/tlb/ide/search.kt
+++ modules/tlb/src/org/ton/intellij/tlb/ide/search.kt
@@ -4,10 +4,16 @@ import com.intellij.lang.HelpID
 import com.intellij.lang.cacheBuilder.DefaultWordsScanner
 import com.intellij.lang.cacheBuilder.WordsScanner
 import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.ElementDescriptionUtil
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.TokenSet
+import com.intellij.usageView.UsageViewLongNameLocation
+import com.intellij.usageView.UsageViewShortNameLocation
 import org.ton.intellij.tlb.lexer.TlbLexerAdapter
+import org.ton.intellij.tlb.psi.TlbField
+import org.ton.intellij.tlb.psi.TlbImplicitField
 import org.ton.intellij.tlb.psi.TlbNamedElement
+import org.ton.intellij.tlb.psi.TlbResultType
 import org.ton.intellij.tlb.psi.TlbTypes
 
 class TlbFindUsagesProvider : FindUsagesProvider {
@@ -15,9 +21,19 @@ class TlbFindUsagesProvider : FindUsagesProvider {
     override fun canFindUsagesFor(element: PsiElement) = element is TlbNamedElement
 
     override fun getHelpId(element: PsiElement) = HelpID.FIND_OTHER_USAGES
-    override fun getType(element: PsiElement) = ""
-    override fun getDescriptiveName(element: PsiElement) = ""
-    override fun getNodeText(element: PsiElement, useFullName: Boolean) = ""
+
+    override fun getType(element: PsiElement) = when (element) {
+        is TlbResultType    -> "Type"
+        is TlbImplicitField -> "Implicit Field"
+        is TlbField         -> "Field"
+        else                -> ""
+    }
+
+    override fun getDescriptiveName(element: PsiElement) =
+        ElementDescriptionUtil.getElementDescription(element, UsageViewLongNameLocation.INSTANCE)
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean) =
+        ElementDescriptionUtil.getElementDescription(element, UsageViewShortNameLocation.INSTANCE)
 }
 
 class TlbWordScanner : DefaultWordsScanner(

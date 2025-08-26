2069636298285fb4260b88f65c6d15a319172bdd
i582 <51853996+i582@users.noreply.github.com>
2025-08-26 14:48:55 +0400

    feat(tolk/documentation): add inline rendered view for Tolk documentation and initial documentation for functions
    Fixes #406
    Fixes #407

---
+++ modules/tolk/src/org/ton/intellij/tolk/doc/DocumentationUtils.kt
@@ -0,0 +1,97 @@
+package org.ton.intellij.tolk.doc
+
+import com.intellij.lang.documentation.DocumentationSettings
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.openapi.editor.richcopy.HtmlSyntaxInfoUtil
+import io.ktor.util.*
+import org.ton.intellij.tolk.ide.colors.TolkColor
+
+object DocumentationUtils {
+    private fun loadKey(key: TextAttributesKey): TextAttributes =
+        EditorColorsManager.getInstance().globalScheme.getAttributes(key)!!
+
+    val asKeyword = loadKey(TolkColor.KEYWORD.textAttributesKey)
+    val asIdentifier = loadKey(DefaultLanguageHighlighterColors.IDENTIFIER)
+    val asBuiltin = loadKey(TolkColor.PRIMITIVE.textAttributesKey)
+    val asType = loadKey(TolkColor.PRIMITIVE.textAttributesKey)
+    val asParameter = loadKey(TolkColor.PARAMETER.textAttributesKey)
+    val asString = loadKey(TolkColor.STRING.textAttributesKey)
+    val asNumber = loadKey(TolkColor.NUMBER.textAttributesKey)
+    val asField = loadKey(TolkColor.FIELD.textAttributesKey)
+    val asComment = loadKey(TolkColor.LINE_COMMENT.textAttributesKey)
+    val asParen = loadKey(TolkColor.PARENTHESES.textAttributesKey)
+    val asBraces = loadKey(TolkColor.BRACES.textAttributesKey)
+    val asBrackets = loadKey(TolkColor.BRACKETS.textAttributesKey)
+    val asComma = loadKey(TolkColor.COMMA.textAttributesKey)
+    val asDot = loadKey(TolkColor.DOT.textAttributesKey)
+
+    val asFunction = loadKey(TolkColor.FUNCTION.textAttributesKey)
+    val asAnnotation = loadKey(TolkColor.ANNOTATION.textAttributesKey)
+    val asStruct = loadKey(TolkColor.STRUCT.textAttributesKey)
+    val asTypeAlias = loadKey(TolkColor.TYPE_ALIAS.textAttributesKey)
+    val asPrimitive = loadKey(TolkColor.PRIMITIVE.textAttributesKey)
+    val asConst = loadKey(TolkColor.CONSTANT.textAttributesKey)
+
+    @Suppress("UnstableApiUsage")
+    fun StringBuilder.colorize(text: String, attrs: TextAttributes, noHtml: Boolean = false) {
+        if (noHtml) {
+            append(text)
+            return
+        }
+
+        HtmlSyntaxInfoUtil.appendStyledSpan(
+            this, attrs, text.escapeHTML(),
+            DocumentationSettings.getHighlightingSaturation(false)
+        )
+    }
+
+    fun StringBuilder.part(text: String?) {
+        if (text.isNullOrEmpty()) {
+            return
+        }
+        append(text)
+        append(" ")
+    }
+
+    fun StringBuilder.part(text: String?, attrs: TextAttributes) {
+        if (text == null) {
+            return
+        }
+        colorize(text, attrs)
+        append(" ")
+    }
+
+    fun StringBuilder.appendNotNull(text: String?) {
+        if (text == null) {
+            return
+        }
+        append(text)
+    }
+
+    fun StringBuilder.line(text: String?) {
+        if (text == null) {
+            return
+        }
+        append(text)
+        append("\n")
+    }
+
+    fun StringBuilder.monospaced(text: String, attrs: TextAttributes? = null, noHtml: Boolean = false) {
+        append("<code>")
+        if (attrs != null) {
+            colorize(text, attrs, noHtml)
+        } else {
+            append(text)
+        }
+        append("</code>")
+    }
+
+    fun colorize(text: String, attrs: TextAttributes, noHtml: Boolean = false): String {
+        val sb = StringBuilder()
+        sb.colorize(text, attrs, noHtml)
+        return sb.toString()
+    }
+}
--- modules/tolk/src/org/ton/intellij/tolk/doc/TolkDocumentationProvider.kt
+++ modules/tolk/src/org/ton/intellij/tolk/doc/TolkDocumentationProvider.kt
@@ -1,67 +1,255 @@
 package org.ton.intellij.tolk.doc
 
-import com.intellij.model.Pointer
-import com.intellij.openapi.util.NlsContexts
-import com.intellij.platform.backend.documentation.DocumentationResult
-import com.intellij.platform.backend.documentation.DocumentationTarget
-import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
-import com.intellij.platform.backend.presentation.TargetPresentation
+import com.intellij.lang.documentation.AbstractDocumentationProvider
+import com.intellij.lang.documentation.DocumentationMarkup
+import com.intellij.psi.PsiDocCommentBase
 import com.intellij.psi.PsiElement
-import com.intellij.psi.createSmartPointer
+import com.intellij.psi.PsiFile
+import com.intellij.psi.SyntaxTraverser
+import org.ton.intellij.tolk.doc.DocumentationUtils.appendNotNull
+import org.ton.intellij.tolk.doc.DocumentationUtils.asAnnotation
+import org.ton.intellij.tolk.doc.DocumentationUtils.asComma
+import org.ton.intellij.tolk.doc.DocumentationUtils.asDot
+import org.ton.intellij.tolk.doc.DocumentationUtils.asFunction
+import org.ton.intellij.tolk.doc.DocumentationUtils.asKeyword
+import org.ton.intellij.tolk.doc.DocumentationUtils.asNumber
+import org.ton.intellij.tolk.doc.DocumentationUtils.asParameter
+import org.ton.intellij.tolk.doc.DocumentationUtils.asParen
+import org.ton.intellij.tolk.doc.DocumentationUtils.asPrimitive
+import org.ton.intellij.tolk.doc.DocumentationUtils.asString
+import org.ton.intellij.tolk.doc.DocumentationUtils.asStruct
+import org.ton.intellij.tolk.doc.DocumentationUtils.asTypeAlias
+import org.ton.intellij.tolk.doc.DocumentationUtils.colorize
+import org.ton.intellij.tolk.doc.DocumentationUtils.line
+import org.ton.intellij.tolk.doc.DocumentationUtils.part
+import org.ton.intellij.tolk.doc.psi.TolkDocComment
+import org.ton.intellij.tolk.highlighting.TolkSyntaxHighlighter
+import org.ton.intellij.tolk.psi.TOLK_KEYWORDS
+import org.ton.intellij.tolk.psi.TOLK_NUMBERS
+import org.ton.intellij.tolk.psi.TOLK_STRING_LITERALS
+import org.ton.intellij.tolk.psi.TolkAnnotation
+import org.ton.intellij.tolk.psi.TolkDocOwner
+import org.ton.intellij.tolk.psi.TolkExpression
+import org.ton.intellij.tolk.psi.TolkFile
 import org.ton.intellij.tolk.psi.TolkFunction
-import org.ton.intellij.tolk.psi.TolkNamedElement
+import org.ton.intellij.tolk.psi.TolkParameter
+import org.ton.intellij.tolk.psi.TolkSelfParameter
+import org.ton.intellij.tolk.psi.TolkStruct
+import org.ton.intellij.tolk.psi.TolkTypeDef
+import org.ton.intellij.tolk.psi.impl.hasReceiver
+import org.ton.intellij.tolk.psi.impl.receiverTy
+import org.ton.intellij.tolk.psi.impl.returnTy
+import org.ton.intellij.tolk.type.TolkIntTyFamily
+import org.ton.intellij.tolk.type.TolkPrimitiveTy
+import org.ton.intellij.tolk.type.TolkTy
+import org.ton.intellij.tolk.type.TolkTyStruct
+import org.ton.intellij.tolk.type.render
+import java.util.function.Consumer
+import kotlin.math.max
 
-class TolkDocumentationProvider : PsiDocumentationTargetProvider {
-    override fun documentationTarget(element: PsiElement, originalElement: PsiElement?): DocumentationTarget? {
-        if (element is TolkFunction) {
-            return TolkDocumentationTarget(element, originalElement)
+class TolkDocumentationProvider : AbstractDocumentationProvider() {
+    override fun generateDoc(element: PsiElement?, originalElement: PsiElement?) = when (element) {
+        is TolkFunction -> element.generateDoc()
+        else            -> null
+    }
+
+    override fun collectDocComments(file: PsiFile, sink: Consumer<in PsiDocCommentBase>) {
+        if (file !is TolkFile) return
+        for (element in SyntaxTraverser.psiTraverser(file)) {
+            if (element is TolkDocComment && element.owner != null && element.text.startsWith("///")) {
+                sink.accept(element)
+            }
         }
-        return null
+    }
+
+    override fun generateRenderedDoc(comment: PsiDocCommentBase): String? {
+        return (comment as? TolkDocComment)?.renderHtml()
     }
 }
 
-@Suppress("UnstableApiUsage")
-class TolkDocumentationTarget(
-    val element: PsiElement,
-    val originalElement: PsiElement?
-) : DocumentationTarget {
-    override fun createPointer(): Pointer<out DocumentationTarget> {
-        val elementPtr = element.createSmartPointer()
-        val originalElementPtr = originalElement?.createSmartPointer()
-        return Pointer {
-            val element = elementPtr.dereference() ?: return@Pointer null
-            TolkDocumentationTarget(element, originalElementPtr?.dereference())
+fun TolkTy.generateDoc(): String {
+    when (this) {
+        is TolkIntTyFamily -> return buildString { colorize(render(), asPrimitive) }
+        is TolkPrimitiveTy -> return buildString { colorize(render(), asPrimitive) }
+        is TolkTyStruct    -> return buildString { colorize(render(), asStruct) }
+        is TolkTypeDef     -> return buildString { colorize(render(), asTypeAlias) }
+    }
+    return render()
+}
+
+fun TolkFunction.generateDoc(): String {
+    val parameters = parameterList?.parameterList ?: emptyList()
+    val returnType = returnTy
+
+    return buildString {
+        append(DocumentationMarkup.DEFINITION_START)
+
+        line(annotations.annotations().generateDoc())
+
+        part("fun", asKeyword)
+
+        val receiverTy = receiverTy
+        if (hasReceiver) {
+            appendNotNull(receiverTy.generateDoc())
+            colorize(".", asDot)
         }
-    }
 
-    override fun computePresentation(): TargetPresentation {
-        val presentableText = (element as? TolkNamedElement)?.name ?: element.text
-        return TargetPresentation.builder(presentableText)
-            .icon(element.getIcon(0))
-            .presentation()
-    }
+        colorize(name ?: "", asFunction)
 
-    override fun computeDocumentationHint(): @NlsContexts.HintText String? {
-        return computeLocalDocumentation(element, originalElement, true)
-    }
+        append(parameters.generateDoc(parameterList?.selfParameter))
+        append(": ")
+        appendNotNull(returnType.generateDoc())
 
-    override fun computeDocumentation(): DocumentationResult? {
-        val html = computeLocalDocumentation(element, originalElement, false) ?: return null
-        return DocumentationResult.documentation(html)
+        append(DocumentationMarkup.DEFINITION_END)
+        generateCommentsPart(this@generateDoc)
     }
 }
 
-private fun computeLocalDocumentation(
-    element: PsiElement,
-    originalElement: PsiElement?,
-    quickNavigation: Boolean
-): String? {
-    when(element) {
-        is TolkFunction -> {
-            val doc = element.doc ?: return null
-            return doc.text.replace("* ", "").removeSurrounding("/**", " */").trimIndent()
+fun Sequence<TolkAnnotation>.generateDoc(): String {
+    return joinToString("\n") { attr ->
+        attr.generateDoc()
+    }
+}
+
+fun TolkAnnotation.generateDoc(): String {
+    val name = identifier?.text ?: ""
+    val arguments = argumentList?.argumentList ?: emptyList()
+    return buildString {
+        colorize("@", asAnnotation)
+        colorize(name, asAnnotation)
+        if (arguments.isNotEmpty()) {
+            colorize("(", asParen)
+            arguments.forEachIndexed { index, argument ->
+                append(argument.expression.generateDoc())
+                if (index != arguments.size - 1) {
+                    append(", ")
+                }
+            }
+            colorize(")", asParen)
+        }
+    }
+}
+
+private fun List<TolkParameter>.generateDoc(selfParameter: TolkSelfParameter?): String {
+    val params = this
+
+    if (params.isEmpty()) {
+        return buildString { colorize("()", asParen) }
+    }
+
+    fun StringBuilder.renderSelfParameter() {
+        if (selfParameter != null) {
+            if (selfParameter.isMutable) {
+                colorize("mutate ", asKeyword)
+            }
+            colorize("self", asKeyword)
+            colorize(", ", asComma)
         }
     }
 
-    return null
+    if (params.size == 1) {
+        val param = params.first()
+        return buildString {
+            colorize("(", asParen)
+            renderSelfParameter()
+            append(param.generateDocForMethod())
+            colorize(")", asParen)
+        }
+    }
+
+    val paramNameMaxWidth = params.maxOfOrNull { it.name?.length ?: 0 } ?: 0
+
+    return buildString {
+        colorize("(", asParen)
+        append("\n")
+        if (selfParameter != null) {
+            append("   ")
+        }
+        renderSelfParameter()
+        if (selfParameter != null) {
+            append("\n")
+        }
+        append(
+            params.joinToString(",\n") { param ->
+                buildString {
+                    append("   ")
+
+                    val name = param.name
+                    if (name != null) {
+                        colorize(name, asParameter)
+                    }
+                    append(": ")
+                    val nameLength = name?.length ?: 0
+                    append("".padEnd(max(paramNameMaxWidth - nameLength, 0)))
+                    append(param.type?.generateDoc())
+                }
+            } + ","
+        )
+        append("\n")
+        colorize(")", asParen)
+    }
+}
+
+private fun TolkParameter.generateDocForMethod(): String {
+    return buildString {
+        val name = name
+        if (name != null) {
+            colorize(name, asParameter)
+            append(": ")
+        }
+        append(type?.generateDoc())
+    }
+}
+
+fun StringBuilder.generateCommentsPart(element: TolkDocOwner?) {
+    val commentsList = listOf(element?.doc)
+    if (commentsList.any { it is TolkDocComment }) {
+        append(DocumentationMarkup.CONTENT_START)
+        for (comment in commentsList) {
+            if (comment is TolkDocComment) {
+                append(comment.renderHtml())
+                append("\n")
+            }
+        }
+        append(DocumentationMarkup.CONTENT_END)
+        return
+    }
+}
+
+fun TolkExpression.generateDoc(): String {
+    val text = text
+    val highlighter = TolkSyntaxHighlighter()
+    val lexer = highlighter.highlightingLexer
+    val builder = StringBuilder()
+    lexer.start(text)
+    while (lexer.tokenType != null) {
+        val type = lexer.tokenType
+        val tokenText = lexer.tokenText
+        val keyword = TOLK_KEYWORDS.contains(type)
+        val number = TOLK_NUMBERS.contains(type)
+        val string = TOLK_STRING_LITERALS.contains(type)
+        val booleanLiteral = tokenText == "true" || tokenText == "false"
+        val builtinFunctions = tokenText == "ton" || tokenText == "address"
+        val primitiveType = TolkPrimitiveTy.fromName(tokenText)
+
+        if (tokenText.contains("\n")) {
+            builder.append("...")
+            break
+        }
+
+        builder.append(
+            when {
+                keyword               -> colorize(tokenText, asKeyword)
+                number                -> colorize(tokenText, asNumber)
+                string                -> colorize(tokenText, asString)
+                booleanLiteral        -> colorize(tokenText, asKeyword)
+                primitiveType != null -> colorize(tokenText, asPrimitive)
+                builtinFunctions      -> colorize(tokenText, asFunction)
+                else                  -> tokenText
+            }
+        )
+        lexer.advance()
+    }
+
+    return builder.toString()
 }

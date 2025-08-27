47999ca0f5958e853a6be8040338a8b9822136ae
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-08-27 23:34:40 +0400

    feat(func): add indent settings (#455)
    Fixes #186

---
+++ modules/func/src/org/ton/intellij/func/ide/formatter/FuncCodeStyleSettings.kt
@@ -0,0 +1,6 @@
+package org.ton.intellij.func.ide.formatter
+
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings
+
+class FuncCodeStyleSettings(settings: CodeStyleSettings) : CustomCodeStyleSettings("FuncCodeStyleSettings", settings)
---
+++ modules/func/src/org/ton/intellij/func/ide/formatter/FuncCodeStyleSettingsProvider.kt
@@ -0,0 +1,29 @@
+package org.ton.intellij.func.ide.formatter
+
+import com.intellij.application.options.CodeStyleAbstractConfigurable
+import com.intellij.application.options.CodeStyleAbstractPanel
+import com.intellij.application.options.TabbedLanguageCodeStylePanel
+import com.intellij.psi.codeStyle.CodeStyleConfigurable
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.CodeStyleSettingsProvider
+import org.ton.intellij.func.FuncLanguage
+
+class FuncCodeStyleSettingsProvider : CodeStyleSettingsProvider() {
+    override fun createCustomSettings(settings: CodeStyleSettings) = FuncCodeStyleSettings(settings)
+
+    override fun getConfigurableDisplayName() = "FunC"
+
+    override fun createConfigurable(
+        settings: CodeStyleSettings,
+        modelSettings: CodeStyleSettings,
+    ): CodeStyleConfigurable {
+        return object : CodeStyleAbstractConfigurable(settings, modelSettings, this.configurableDisplayName) {
+            override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel {
+                return FuncCodeStyleMainPanel(currentSettings, settings)
+            }
+        }
+    }
+
+    private class FuncCodeStyleMainPanel(currentSettings: CodeStyleSettings, settings: CodeStyleSettings) :
+        TabbedLanguageCodeStylePanel(FuncLanguage, currentSettings, settings)
+}

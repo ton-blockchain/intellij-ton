2a5ab0ddb5016260e5393ea696aee0f38bc193c2
i582 <51853996+i582@users.noreply.github.com>
2025-08-27 21:58:36 +0400

    fix(tlb/colors): fix TL-B color settings example
    Fixes #451

--- modules/tlb/src/org/ton/intellij/tlb/ide/colors.kt
+++ modules/tlb/src/org/ton/intellij/tlb/ide/colors.kt
@@ -16,8 +16,8 @@ enum class TlbColor(
     displayName: String,
     default: TextAttributesKey,
 ) {
-    COMMENT("Comment", Defaults.LINE_COMMENT),
-    DOCUMENTATION("Documentation", Defaults.DOC_COMMENT),
+    COMMENT("Comments//Line", Defaults.LINE_COMMENT),
+    DOCUMENTATION("Comments//Documentation", Defaults.DOC_COMMENT),
 
     BRACES("Braces", Defaults.BRACES),
     BRACKETS("Brackets", Defaults.BRACKETS),
@@ -26,13 +26,13 @@ enum class TlbColor(
     OPERATION_SIGN("Operation Sign", Defaults.OPERATION_SIGN),
 
     NUMBER("Number", Defaults.NUMBER),
-    CONSTRUCTOR_NAME("Constructor name", Defaults.STATIC_METHOD),
-    HEX_TAG("HEX Tag", Defaults.STATIC_FIELD),
-    BINARY_TAG("Binary Tag", Defaults.STATIC_FIELD),
-    FIELD_NAME("Field name", Defaults.INSTANCE_FIELD),
-    TYPE_PARAMETER("Type parameter", TextAttributesKey.find("TYPE_PARAMETER_NAME_ATTRIBUTES")),
-    RESULT_TYPE_NAME("Result type name", Defaults.KEYWORD),
-    IMPLICIT_FIELD_NAME("Implicit field name", XmlHighlighterColors.HTML_ATTRIBUTE_VALUE),
+    CONSTRUCTOR_NAME("Type//Constructor name", Defaults.STATIC_METHOD),
+    HEX_TAG("Type//Hex Tag", Defaults.STATIC_FIELD),
+    BINARY_TAG("Type//Binary Tag", Defaults.STATIC_FIELD),
+    FIELD_NAME("Type//Field name", Defaults.INSTANCE_FIELD),
+    TYPE_PARAMETER("Type//Type parameter", TextAttributesKey.find("TYPE_PARAMETER_NAME_ATTRIBUTES")),
+    RESULT_TYPE_NAME("Type//Result type name", Defaults.KEYWORD),
+    IMPLICIT_FIELD_NAME("Type//Implicit field name", XmlHighlighterColors.HTML_ATTRIBUTE_VALUE),
     BUILTIN_TYPE("Builtin type", XmlHighlighterColors.HTML_ATTRIBUTE_VALUE),
     IDENTIFIER("Identifier",  XmlHighlighterColors.HTML_ATTRIBUTE_NAME),
     ;
@@ -42,22 +42,16 @@ enum class TlbColor(
     val attributesDescriptor = AttributesDescriptor(displayName, textAttributesKey)
 }
 
-
 class TlbColorSettingsPage : ColorSettingsPage {
     private val DEMO_TEXT by lazy {
         loadTextResource(TlbColorSettingsPage::class.java, "colors/highlighter_example.tlb")
     }
 
-    override fun getAttributeDescriptors(): Array<AttributesDescriptor> = ATTRIBUTE_DESCRIPTORS
+    override fun getAdditionalHighlightingTagToDescriptorMap(): Map<String, TextAttributesKey> = TlbColor.entries.associateBy({ it.name }, { it.textAttributesKey })
+    override fun getAttributeDescriptors(): Array<AttributesDescriptor> = TlbColor.entries.map { it.attributesDescriptor }.toTypedArray()
     override fun getColorDescriptors(): Array<ColorDescriptor> = ColorDescriptor.EMPTY_ARRAY
     override fun getDisplayName(): String = TlbLanguage.displayName
     override fun getIcon(): Icon = TlbIcons.FILE
     override fun getHighlighter(): SyntaxHighlighter = TlbSyntaxHighlighter
     override fun getDemoText(): String = DEMO_TEXT
-
-    override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey>? = null
-
-    companion object {
-        val ATTRIBUTE_DESCRIPTORS = TlbColor.values().map { it.attributesDescriptor }.toTypedArray()
-    }
 }

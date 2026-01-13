d1f5e3e9aad9a5e758e6b8fe4317ce865cf09d7e
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-01-13 15:42:57 +0400

    fix(acton): fix wallet address regex

--- modules/acton/src/org/ton/intellij/acton/ide/ActonTonAddressConsoleFilter.kt
+++ modules/acton/src/org/ton/intellij/acton/ide/ActonTonAddressConsoleFilter.kt
@@ -61,7 +61,7 @@ class ActonTonAddressConsoleFilter : Filter {
 
     companion object {
         private val rawPattern = Regex("""\b-?[0-9]:[0-9a-fA-F]{64}\b""")
-        private val userFriendlyPattern = Regex("""\b[EfUk0][Qf][a-zA-Z0-9_-]{46}\b""")
+        private val userFriendlyPattern = Regex("""\b[EfUk0][Qf][a-zA-Z0-9+_-]{46}\b""")
     }
 }
 

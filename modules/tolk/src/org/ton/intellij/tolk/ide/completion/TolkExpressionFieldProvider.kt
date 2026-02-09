60246b865baab91419aa84eb1031330c78225736
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-02-09 16:59:55 +0400

    fix(tolk): use `createEmptyMap()` as a default value for map field (#682)
    Fixes #675

--- modules/tolk/src/org/ton/intellij/tolk/ide/completion/TolkExpressionFieldProvider.kt
+++ modules/tolk/src/org/ton/intellij/tolk/ide/completion/TolkExpressionFieldProvider.kt
@@ -171,6 +171,9 @@ object TolkExpressionFieldProvider : TolkCompletionProvider() {
         }
 
         if (type is TolkTyStruct) {
+            if (type.psi.name == "map") {
+                return "createEmptyMap()"
+            }
             if (type.typeArguments.isNotEmpty() && type.psi.name == "Cell") {
                 // `Cell<T>` -> `T {}.toCell()` or `defaultOf(T).toCell()`
                 val arg = type.typeArguments[0]

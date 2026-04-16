2b1db70ff69cb8d42e9d4f161e1c546a69cbc7b2
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-04-16 21:37:07 +0400

    feat(acton): support broadcasting with `--net` flag (#710)

--- modules/tolk/src/org/ton/intellij/tolk/ide/linemarker/TolkRunScriptLineMarkerProvider.kt
+++ modules/tolk/src/org/ton/intellij/tolk/ide/linemarker/TolkRunScriptLineMarkerProvider.kt
@@ -22,8 +22,10 @@ class TolkRunScriptLineMarkerProvider : RunLineMarkerContributor() {
         val parent = element.parent
         if (parent is TolkFunction && parent.name == "main") {
             val actions = listOf(
-                RunActonScriptAction(false),
-                RunActonScriptAction(true)
+                RunActonScriptAction(),
+                RunActonScriptBroadcastAction("testnet"),
+                RunActonScriptBroadcastAction("mainnet"),
+                RunActonScriptBroadcastAction("localnet"),
             )
             return Info(AllIcons.RunConfigurations.TestState.Run, (actions + ExecutorAction.getActions(0)).toTypedArray()) { "Run Script" }
         }
@@ -31,14 +33,23 @@ class TolkRunScriptLineMarkerProvider : RunLineMarkerContributor() {
         return null
     }
 
-    private class RunActonScriptAction(private val broadcast: Boolean) : AnAction(
-        if (broadcast) "Broadcast" else "Emulate",
+    private class RunActonScriptAction : AnAction("Emulate", null, AllIcons.Ide.LocalScope) {
+        override fun actionPerformed(e: AnActionEvent) {
+            val context = ConfigurationContext.getFromContext(e.dataContext)
+            val producer = ActonRunScriptConfigurationProducer()
+            val configurationFromContext = producer.findOrCreateConfigurationFromContext(context) ?: return
+            ExecutionUtil.runConfiguration(configurationFromContext.configurationSettings, DefaultRunExecutor.getRunExecutorInstance())
+        }
+    }
+
+    private class RunActonScriptBroadcastAction(private val network: String) : AnAction(
+        "Broadcast to ${network.replaceFirstChar(Char::titlecase)}",
         null,
-        if (broadcast) AllIcons.General.Export else AllIcons.Ide.LocalScope
+        AllIcons.General.Export
     ) {
         override fun actionPerformed(e: AnActionEvent) {
             val context = ConfigurationContext.getFromContext(e.dataContext)
-            val producer = if (broadcast) ActonRunScriptBroadcastConfigurationProducer() else ActonRunScriptConfigurationProducer()
+            val producer = ActonRunScriptBroadcastConfigurationProducer(network)
             val configurationFromContext = producer.findOrCreateConfigurationFromContext(context) ?: return
             ExecutionUtil.runConfiguration(configurationFromContext.configurationSettings, DefaultRunExecutor.getRunExecutorInstance())
         }

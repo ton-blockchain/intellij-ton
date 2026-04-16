1b911d775a4884589e327a3b302dffa778949d57
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-04-16 17:42:35 +0400

    feat(acton): support all options in projext wizard for Acton

--- modules/acton/src/org/ton/intellij/acton/cli/ActonCommand.kt
+++ modules/acton/src/org/ton/intellij/acton/cli/ActonCommand.kt
@@ -122,6 +122,8 @@ sealed class ActonCommand(val name: String) {
         var template: String? = null,
         var app: Boolean = false,
         var license: String? = null,
+        var hooks: Boolean = false,
+        var agents: Boolean = false,
     ) : ActonCommand("new") {
         override fun getArguments(): List<String> = buildList {
             add(path)
@@ -143,6 +145,12 @@ sealed class ActonCommand(val name: String) {
             license?.let {
                 add("--license")
                 add(it)
+            }
+            if (hooks) {
+                add("--hooks")
+            }
+            if (agents) {
+                add("--agents")
             }
         }
     }
